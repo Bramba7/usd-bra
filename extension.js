6061a5ab27ba811b995e16822caa17880f6b086b
Fernando Brambilla de Mello <43076031+Bramba7@users.noreply.github.com>
2022-10-04 14:41:44 -0700

    change indicator's place

--- extension.js
+++ extension.js
@@ -99,7 +99,7 @@ async function handle_request_dollar_api() {
  
             // Sent text in Widget
             panelButtonText = new St.Label({
-                text: upDownIcon + " BRL: $" + dollarQuotation,
+                text: " BRL: $" + dollarQuotation + " " + upDownIcon,
                 y_align: Clutter.ActorAlign.CENTER,
             });
             panelButton.set_child(panelButtonText);
