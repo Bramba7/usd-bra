1845e5f6da08b1f8fc20689df2b39f16629ff2f1
Fernando Brambilla <bramba@outlook.com>
2023-11-01 15:29:07 -0500

    change the arrow color

--- extension.js
+++ extension.js
@@ -16,10 +16,9 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
-'use strict';
+"use strict";
 
 const { St, Gio, Clutter, Soup, GLib } = imports.gi;
-
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const Main = imports.ui.main;
@@ -32,90 +31,111 @@ let sourceId = null;
 
 // Start application
 function init() {
-//    log(`initializing ${Me.metadata.name}`);
+  // Initialization if needed
 }
 
 // Add the button to the panel
 function enable() {
-    panelButton = new St.Bin({
-        style_class: "panel-button",
-    });
+  panelButton = new St.Bin({
+    style_class: "panel-button",
+  });
 
+  handle_request_dollar_api();
+  Main.panel._centerBox.insert_child_at_index(panelButton, 0);
+  sourceId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 30, () => {
     handle_request_dollar_api();
-    Main.panel._centerBox.insert_child_at_index(panelButton, 0);
-    sourceId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 30, () => {
-        handle_request_dollar_api();
-        return GLib.SOURCE_CONTINUE;
-    });
+    return GLib.SOURCE_CONTINUE;
+  });
 }
 
 // Remove the added button from panel
 function disable() {
-    panelButtonText = null;
-    session = null;
-    Main.panel._centerBox.remove_child(panelButton);
+  panelButtonText = null;
+  session = null;
+  Main.panel._centerBox.remove_child(panelButton);
 
-    if (panelButton) {
-        panelButton.destroy();
-        panelButton = null;
-    }
+  if (panelButton) {
+    panelButton.destroy();
+    panelButton = null;
+  }
 
-    if (sourceId) {
-        GLib.Source.remove(sourceId);
-        sourceId = null;
-    }
+  if (sourceId) {
+    GLib.Source.remove(sourceId);
+    sourceId = null;
+  }
 }
 
 // Handle Requests API Dollar
 async function handle_request_dollar_api() {
-    let dollarQuotation = null;
-    let upDown = null;
-    let upDownIcon = null;
+  let dollarQuotation = null;
+  let upDown = null;
+  let upDownIcon = null;
 
-    try {
-        // Create a new Soup Session
-        if (!session) {
-            session = new Soup.Session({ timeout: 10 });
-        }
-
-        // Create body of Soup request
-        let message = Soup.Message.new_from_encoded_form(
-            "GET", "https://economia.awesomeapi.com.br/last/USD-BRL", Soup.form_encode_hash({}));
-
-        // Send Soup request to API Server
-        await session.send_and_read_async(message, GLib.PRIORITY_DEFAULT, null, (_, r0) => {
-            let text = session.send_and_read_finish(r0);
-            let response = new TextDecoder().decode(text.get_data());
-            const body_response = JSON.parse(response);
-
-            // Get the value of Dollar Quotation
-            upDown = body_response["USDBRL"]["varBid"];     
-            dollarQuotation = body_response["USDBRL"]["bid"];
-            dollarQuotation = dollarQuotation.split(".");
-            dollarQuotation = dollarQuotation[0] + "." + dollarQuotation[1].substring(0, 2);
-                 
-            
-            parseFloat(upDown) > 0 ? upDownIcon = "⬆" : upDownIcon = "⬇";
- 
-            // Sent text in Widget
-            panelButtonText = new St.Label({
-                text: " BRL: $" + dollarQuotation + " " + upDownIcon,
-                y_align: Clutter.ActorAlign.CENTER,
-            });
-            panelButton.set_child(panelButtonText);
-
-            // Finish Soup Session
-            session.abort();
-            text = undefined;
-            response = undefined;
-        });
-    } catch (error) {
-        log(`Traceback Error in [handle_request_dollar_api]: ${error}`);
-        panelButtonText = new St.Label({
-            text: "BRL: $" + _dollarQuotation +  " * ",
-            y_align: Clutter.ActorAlign.CENTER,
-        });
-        panelButton.set_child(panelButtonText);
-        session.abort();
+  try {
+    // Create a new Soup Session
+    if (!session) {
+      session = new Soup.Session({ timeout: 10 });
     }
+
+    // Create body of Soup request
+    let message = Soup.Message.new_from_encoded_form(
+      "GET",
+      "https://economia.awesomeapi.com.br/last/USD-BRL",
+      Soup.form_encode_hash({})
+    );
+
+    // Send Soup request to API Server
+    await session.send_and_read_async(
+      message,
+      GLib.PRIORITY_DEFAULT,
+      null,
+      (_, r0) => {
+        let text = session.send_and_read_finish(r0);
+        let response = new TextDecoder().decode(text.get_data());
+        const body_response = JSON.parse(response);
+
+        // Get the value of Dollar Quotation
+        upDown = body_response["USDBRL"]["varBid"];
+        dollarQuotation = body_response["USDBRL"]["bid"];
+        dollarQuotation = dollarQuotation.split(".");
+        dollarQuotation =
+          dollarQuotation[0] + "." + dollarQuotation[1].substring(0, 2);
+
+        parseFloat(upDown) > 0 ? (upDownIcon = "⬆") : (upDownIcon = "⬇");
+        let rateDisplay = "BRL: $" + dollarQuotation + " ";
+        let iconDisplay = upDownIcon;
+        let iconColor = upDown > 0 ? "darkgreen" : "darkred";
+
+        let label1 = new St.Label({
+          text: rateDisplay,
+          y_align: Clutter.ActorAlign.CENTER,
+        });
+
+        let label2 = new St.Label({
+          text: iconDisplay,
+          y_align: Clutter.ActorAlign.CENTER,
+          style: `color: ${iconColor};`,
+        });
+
+        panelButtonText = new St.BoxLayout();
+        panelButtonText.add_child(label1);
+        panelButtonText.add_child(label2);
+
+        panelButton.set_child(panelButtonText);
+        // Finish Soup Session
+        session.abort();
+        text = undefined;
+        response = undefined;
+      }
+    );
+  } catch (error) {
+    log(`Traceback Error in [handle_request_dollar_api]: ${error}`);
+    panelButtonText = new St.Label({
+      text: "BRL: $" + _dollarQuotation + " * ",
+      y_align: Clutter.ActorAlign.CENTER,
+    });
+    panelButton.set_child(panelButtonText);
+    session.abort();
+  }
 }
+
